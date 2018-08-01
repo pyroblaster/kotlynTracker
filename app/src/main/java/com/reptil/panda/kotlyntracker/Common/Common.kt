375d17f1cd460cf1e4bd8efad8b92c374de209e8
pyroblaster <abartulovic@etfos.hr>
2018-08-02 00:45:05 +0200

    ne radi

---
+++ app/src/main/java/com/reptil/panda/kotlyntracker/Common/Common.kt
@@ -0,0 +1,7 @@
+package com.reptil.panda.kotlyntracker.Common
+
+object Common{
+    var imageUrl = "https://res.cloudinary.com/dxi90ksom/image/upload/"
+
+    var MAX_COIN_LOAD = 1000
+}
---
+++ app/src/main/java/com/reptil/panda/kotlyntracker/Interface/ILoadMore.kt
@@ -0,0 +1,5 @@
+package com.reptil.panda.kotlyntracker.Interface
+
+interface ILoadMore {
+    fun onLoadMore()
+}
